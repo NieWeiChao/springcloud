56727c7725e00968211927d5a292d0433ca446b3
BillNwc <nwc1071661970@163.com>
2018-10-15 10:47:26 +0800

    menu 内存递归

--- javaStudy/src/main/java/site/wattsnwc/menudemo/MenuUtil.java
+++ javaStudy/src/main/java/site/wattsnwc/menudemo/MenuUtil.java
@@ -12,19 +12,24 @@ public class MenuUtil {
     private static void buildMenuTree(List<Menu> firstMenus, List<Menu> menus,final List<String> needCheck ){
         //递归并行,使用线程安全集合处理
         List<Menu> copyMenus = new CopyOnWriteArrayList<>(menus);
+        //先找到PID等于当前节点ID的集合
         firstMenus.forEach(menu ->
                 copyMenus.stream()
+                        //过滤等于PID的节点
                         .filter(otherMenu -> otherMenu.getPId().equals(menu.getId()))
+                        //peek赋值是否选中
                         .peek(otherMenu -> {
                             if(needCheck.contains(otherMenu.getName())){
                                 otherMenu.setChecked(true);
                             }
                         })
+                        //遍历节点:添加到子节点,并从剩余节点移除
                         .forEach(otherMenu -> {
                             menu.addSubs(otherMenu);
                             copyMenus.remove(otherMenu);
                         })
         );
+        //子节点不为空,继续递归查找子节点下的子节点
         firstMenus.stream()
                 .filter(menu -> menu.getSubs()!=null)
                 .forEach(menu -> buildMenuTree(menu.getSubs(), copyMenus,needCheck));
@@ -32,11 +37,16 @@ public class MenuUtil {
 
     static List<Menu> buildTree(List<Menu> menus,final List<String> needCheck ){
         //取到顶级节点集合或者顶级ID为 ** 的集合
-        List<Menu> firstMenus = menus.stream().filter(menu -> null == menu.getPId()).peek(menu -> {
-            if(needCheck.contains(menu.getName())){
-                menu.setChecked(true);
-            }
-        }).collect(Collectors.toList());
+        List<Menu> firstMenus = menus.stream()
+                .filter(menu -> null == menu.getPId())
+                //peek赋值是否选中
+                .peek(menu -> {
+                    if(needCheck.contains(menu.getName())){
+                        menu.setChecked(true);
+                    }
+                })
+                //集合
+                .collect(Collectors.toList());
         menus.removeAll(firstMenus);
         MenuUtil.buildMenuTree(firstMenus, menus,needCheck);
         return firstMenus;
